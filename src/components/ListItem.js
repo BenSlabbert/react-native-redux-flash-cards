17bcae8ea67468ed573cd839d892d41d4603e0aa
Ben Slabbert <ben@hex.co.za>
2018-01-04 08:16:17 +0200

    adding action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 
 import { CardSection } from './../common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
 
@@ -25,4 +27,7 @@ const styles = {
     }
 };
 
-export default ListItem;
+// passing actions to redux
+// dispatches actions to the redux store
+// and passes them into the component as props
+export default connect(null, actions)(ListItem);
---
+++ src/reducers/SelectionReducer.js
@@ -0,0 +1,5 @@
+export default () => {
+    // must have some initial state
+    // cannot be undefined, can return null
+    return null;
+};
